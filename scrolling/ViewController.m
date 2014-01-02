6d3072c8a7d003ea68f490fa792455bd2cb33bc8
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-01-02 20:26:02 -0200

    scrolling sample implemented

--- scrolling/ViewController.m
+++ scrolling/ViewController.m
@@ -14,10 +14,25 @@
 
 @implementation ViewController
 
+@synthesize scroller;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    self.scroller.scrollEnabled=YES;
+    self.scroller.contentSize=CGSizeMake(320, 3000);
+    // Loop para injetar varios labels para ocupar a altura de 3000
+    
+    for (int i=15; i<3000; i=i+15) {
+        NSLog(@"%d",i);
+        UILabel *msg=[[UILabel alloc]initWithFrame:CGRectMake(80, i, 100, 20)];
+        msg.text=[NSString stringWithFormat:@"%d",i];
+        [self.scroller addSubview:msg];
+    }
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
